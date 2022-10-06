2305d6ed122759aa3c8dff8a5179a7a8ae181132
Pallavi Saxena <saxena.pallavi7211@gmail.com>
2022-10-06 21:38:43 +0530

    stage 4 evaluation done

--- src/stage_04_evaluate.py
+++ src/stage_04_evaluate.py
@@ -1,13 +1,17 @@
-import argparse
 import os
-import shutil
-from tqdm import tqdm
+import joblib
+import argparse
 import logging
+import numpy as np
+import tensorflow as tf
 from src.utils.common import read_yaml, create_directories
-import random
+from src.utils.models import build_model, generate_text
+import warnings
+warnings.filterwarnings('ignore')
+warnings.simplefilter('ignore')
 
 
-STAGE = "STAGE_NAME" ## <<< change stage name 
+STAGE = "STAGE 4" 
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
@@ -21,7 +25,50 @@ def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
+
+    artifacts = config["artifacts"]
+    model_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["MODEL_DIR"])
+    checkpoint_dir = os.path.join(model_dir_path, artifacts["CHECKPOINT_DIR"])
+
+    featurized_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"], artifacts["FEATURIZED_DATA"])
+    vocab_data_file = os.path.join(featurized_data_dir_path, artifacts["VOCAB_DATA"])
+    text_as_int_file = os.path.join(featurized_data_dir_path, artifacts["TEXT_AS_INTEGER"])
+
+    embedding_dim = params["train"]["embedding_dim"]
+    rnn_units = params["train"]["rnn_units"]
+    start_string = params["evaluate"]["start_string"]
+    no_of_chars_to_gen = params["evaluate"]["no_of_chars_to_gen"]
+
+    vocab = joblib.load(vocab_data_file)
+    text_as_int = joblib.load(text_as_int_file)
+    vocab_len = len(vocab)
+    print(f"Vocab length: {vocab_len}")
+    char2idx = {uniChar: idx for idx, uniChar in enumerate(vocab)}
+    idx2char_DICT = {val: key for key, val in char2idx.items()}
+    idx2char = np.array(vocab)
+    
+    # Restoring checkoint - 
+    tf.train.latest_checkpoint(checkpoint_dir)
+
+    model_from_ckpt = build_model(
+    vocab_size = vocab_len,
+    embedding_dim = embedding_dim,
+    rnn_units = rnn_units,
+    batch_size = 1 
+    )
+
+    print(model_from_ckpt.summary())
+
+    model_from_ckpt.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+    model_from_ckpt.build(tf.TensorShape([1, None]))
+
+    print(model_from_ckpt.summary())
+
+    result = generate_text(model=model_from_ckpt, start_string=start_string,
+                            char2idx=char2idx, idx2char=idx2char,
+                            no_of_chars_to_gen=no_of_chars_to_gen)
+    print(result)
+
 
 
 if __name__ == '__main__':
--- src/utils/models.py
+++ src/utils/models.py
@@ -14,3 +14,30 @@ def build_model(
 
 def model_loss(labels, logits):
     return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)  #logits: raw output from dense neurons without any activation function
+
+def generate_text(model, start_string, char2idx, idx2char, no_of_chars_to_gen=1000):
+    # convert the input text to nos.
+    input_val = [char2idx[s] for s in start_string] # text converted to int
+    input_val = tf.expand_dims(input_val, 0) # [] ->> [1, ]
+
+    text_generated = list()
+
+    temperature = 1.0
+
+
+    model.reset_states() # Resetting the previous states if any while predictions.
+    for i in range(no_of_chars_to_gen):
+        predictions = model(input_val)
+
+        predictions = tf.squeeze(predictions, 0)
+
+        predictions = predictions / temperature
+        # print(predictions)
+        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1, 0].numpy() # It will draw some sample out of it
+        # print(predicted_id)
+        
+        input_val = tf.expand_dims([predicted_id], 0)
+
+        text_generated.append(idx2char[predicted_id])
+
+    return start_string + "".join(text_generated)
